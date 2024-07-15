347ab4d8bbc4f3947dc9083d7b90b541f44cdb56
dreammehedi <dreammehedihassan@gmail.com>
2024-07-15 23:01:43 +0600

    Login Page Ui Update

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,64 +1,62 @@
-import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  const [formData, setFormData] = useState({
-    credential: "",
-    pin: "",
-  });
-
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    // Form submission logic goes here
-    history.push("/dashboard");
-  };
-
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-        <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
-        {/* {error && <p className="text-red-500 mb-4">{error}</p>} */}
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label className="block text-gray-700">Mobile Number / Email</label>
-            <input
-              type="text"
-              name="credential"
-              value={formData.credential}
-              onChange={handleChange}
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
-              required
-            />
+    <>
+      <section className="py-10 md:py-12 lg:py-16 bg-gray-100 w-full min-h-screen flex justify-center items-center">
+        <div className="flex items-center justify-center ">
+          <div className="container bg-white p-6 md:p-8 rounded-lg shadow-lg w-full max-w-md">
+            {/* page title */}
+            <h2 className="text-2xl md:text-3xl font-bold mb-6 text-center">
+              Login
+            </h2>
+            {/* {error && <p className="text-red-500 mb-4">{error}</p>} */}
+            <form className="space-y-4">
+              {/* mobile number/email field */}
+              <div>
+                <label className="text-sm text-gray-700">
+                  Mobile Number / Email
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Enter Your Number / Email..."
+                  className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500 my-transition placeholder:text-sm"
+                />
+              </div>
+
+              {/* PIN field */}
+              <div>
+                <label className="text-sm text-gray-700">PIN</label>
+                <input
+                  type="password"
+                  name="pin"
+                  placeholder="Enter Your PIN..."
+                  className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500 my-transition placeholder:text-sm"
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="w-full font-medium bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-700 my-transition"
+              >
+                Login
+              </button>
+            </form>
+
+            <p className="mt-4 text-center text-gray-700 my-transition">
+              {`Don't have an account?`}{" "}
+              <Link
+                to={"/register"}
+                className="my-transition text-blue-500 hover:underline"
+              >
+                Register
+              </Link>
+            </p>
           </div>
-          <div className="mb-4">
-            <label className="block text-gray-700">PIN</label>
-            <input
-              type="password"
-              name="pin"
-              value={formData.pin}
-              onChange={handleChange}
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
-              required
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-700"
-          >
-            Login
-          </button>
-        </form>
-        <p className="mt-4 text-center">
-          {`Don't have an account?`}{" "}
-          <a href="/register" className="text-blue-500 hover:underline">
-            Register
-          </a>
-        </p>
-      </div>
-    </div>
+        </div>
+      </section>
+    </>
   );
 };
 
