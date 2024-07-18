0d55ada17a4df21feda3cae0a39e11eae054f96f
dreammehedi <dreammehedihassan@gmail.com>
2024-07-18 17:05:47 +0600

    Admin All Transaction History Get

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -93,6 +93,36 @@ function AdminDashboard({ user }) {
     const searchData = form.userIdentification.value;
     setUserSearchValue(searchData);
   };
+
+  // get all transactions history
+  const { data: allTransaction = [] } = useQuery({
+    queryKey: ["adminAllTransactionHistory"],
+    queryFn: async () => {
+      try {
+        const response = await AxiosSecure.get(
+          "/admin-all-transactions-history"
+        );
+        const resData = await response.data;
+        return resData;
+      } catch (err) {
+        console.error(err);
+      }
+    },
+  });
+  // Function to format the date
+  const formatDate = (timestamp) => {
+    const date = new Date(timestamp);
+    return date.toLocaleString("en-GB", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: true,
+      timeZone: "Asia/Dhaka",
+    });
+  };
   return (
     <>
       {/* dynamic page title */}
@@ -273,6 +303,64 @@ function AdminDashboard({ user }) {
               <p className="text-sm font-medium font-inter text-red-500">
                 {" "}
                 No Users Found!
+              </p>
+            )}
+          </div>
+
+          {/* transaction history page */}
+          <div className=" bg-white p-4 md:p-6 rounded-lg shadow-lg">
+            <h2 className="text-xl md:text-2xl font-bold mb-4 flex items-center gap-2">
+              Transaction History Information:{" "}
+              <span className="bg-blue-500 text-white p-4 text-base rounded-full size-5 flex justify-center items-center">
+                {allTransaction?.length}
+              </span>
+            </h2>
+
+            {allTransaction?.length > 0 ? (
+              <div className="overflow-x-auto rounded-md">
+                <table className="min-w-full text-xs text-left">
+                  {/* table head */}
+                  <thead className="w-full bg-blue-500  text-white">
+                    <tr className="*:px-4 *:py-3 text-base">
+                      <th>Sender Number</th>
+                      <th>Reciver Number</th>
+                      <th>Amount</th>
+                      <th>Date</th>
+                      <th>Transaction Id</th>
+                      <th>Type</th>
+                    </tr>
+                  </thead>
+
+                  {/* table body */}
+                  <tbody className="font-medium">
+                    {allTransaction?.map((transaction, ind) => {
+                      return (
+                        <tr
+                          key={ind}
+                          className={`${
+                            ind % 2 === 0 ? "bg-blue-200" : "bg-blue-100"
+                          } *:px-4 *:py-2 *:break-words `}
+                        >
+                          <td>{transaction?.senderId}</td>
+                          <td>{transaction?.recipiend}</td>
+                          <td>
+                            <div className="flex items-center gap-1">
+                              <TbCurrencyTaka></TbCurrencyTaka>
+                              {transaction?.amount}
+                            </div>
+                          </td>
+                          <td>{formatDate(transaction?.date)}</td>
+                          <td>{transaction?.transactionId}</td>
+                          <td>{transaction?.type}</td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+              </div>
+            ) : (
+              <p className="text-sm font-medium font-inter text-red-500">
+                No Transaction Found!
               </p>
             )}
           </div>
